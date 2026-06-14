a50fdfd10332663eb55275a12daa92a87617c906
FaridFiansyah <faridfiansyah9@gmail.com>
2026-06-14 13:09:31 +0700

    firebasev2

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,14 +2,14 @@
 // File ini aman tampil di browser, tapi SECURITY RULES Firebase tetap wajib benar.
 // Untuk local test tanpa online room, biarkan enabled=false.
 
-export const firebaseEnabled = false;
+export const firebaseEnabled = true;
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyD_ouG6QtbdS2NiIeBQgpuSsTv2G-vNg2E",
+  authDomain: "duel-manager-f915f.firebaseapp.com",
+  databaseURL: "https://duel-manager-f915f-default-rtdb.firebaseio.com",
+  projectId: "duel-manager-f915f",
+  storageBucket: "duel-manager-f915f.firebasestorage.app",
+  messagingSenderId: "242957046879",
+  appId: "1:242957046879:web:d704ec457b2a19eec437ea",
 };
